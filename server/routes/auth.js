176012e6e7fa406d3385a0ee7dbc5f2b3f53c79d
kapilansh <kapilansh.patil.etrx@ghrce.raisoni.net>
2025-07-31 13:51:00 +0530

    Add user login functionality with JWT authentication

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const brcypt = require("bcryptjs");
+const jwt = require("jsonwebtoken")
+const bcrypt = require("bcryptjs");
 const User = require("../models/User") // Import the User model
 
 // @route POST api/auth/register
@@ -36,6 +37,48 @@ router.post('/register', async (req, res) => {
     } catch (error) {
         console.log(err.message);
         res.status(500).send('Server error')
+    }
+})
+
+// @route POST api/auth/login
+//  @desc Authenticate user & get the token
+//  @access Public
+
+router.post('/login', async (req, res) => {
+    const {email, password} = req.body;
+
+    try {
+        // to check if the user already exists
+        let user = await User.findOne({email});
+        if(!user) {
+            return res.status(404).json({ msg: "Invalid credentials" });
+        }
+
+        // compare the submitted password with the stored hash
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch) {
+            return res.status(404).json({ msg: "Invalid credentials" });
+        }
+
+        // Create and sign the JWT
+        const payload = {
+            user: {
+                id: user.id, // we can later use this id to fetch user-specific data
+            }
+        };
+
+        jwt.sign(
+            payload,
+            process.env.JWT_SECRET,
+            { expiresIn: '5h' }, // the token expires in 5 hours
+            (err, token) => {
+                if (err) throw err;
+                res.json({ token }) 
+            }
+        );
+    } catch (error) {
+        console.error(error.message)
+        res.status(500).send('Server Error')
     }
 })
 
